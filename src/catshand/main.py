b3867cc0eb6ddb2f89c7a241f7ef4ab4f60e4a1a
eufmike <m.cc.shih@gmail.com>
2023-04-02 21:08:57 -0700

    update main

--- src/catshand/main.py
+++ src/catshand/main.py
@@ -2,7 +2,8 @@ import argparse
 from catshand.tools import prjpre, prjpost, audacitypipe, highlight, audmerger, linkparser
 
 def main():
-    parser = argparse.ArgumentParser(description='Main parser')
+    description = "Cat's Hand (catshand) is a toolbox designed for audio editing and production in the team of Tripod Cat's Podcast. Cat's Hand (catshand) is named after the Japanese idiom \"猫の手も借りたい\", which means \"so busy someone wants to borrow even the paw of a cat.\""
+    parser = argparse.ArgumentParser(description=description)
     subparsers = parser.add_subparsers(help='Subparsers help')
 
     prjpre.add_subparser(subparsers)

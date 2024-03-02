aa9125f67cd7740139724efe6afae7d14ac47b49
eufmike <m.cc.shih@gmail.com>
2024-03-01 20:35:26 -0800

    version change

--- setup.py
+++ setup.py
@@ -2,36 +2,35 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-    
+
 setuptools.setup(
-    name='catshand',
-    version='0.3.2',
-    author='Mike Chien-Cheng Shih',
-    author_email='m.cc.shih@gmail.com',
-    description='Tools for Podcast editing',
-    url='https://github.com/eufmike/catshand',
+    name="catshand",
+    version="0.3.3",
+    author="Mike Chien-Cheng Shih",
+    author_email="m.cc.shih@gmail.com",
+    description="Tools for Podcast editing",
+    url="https://github.com/eufmike/catshand",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    license='BSD 2-clause',
-    install_requires=['numpy'],
+    long_description_content_type="text/markdown",
+    license="BSD 2-clause",
+    install_requires=["numpy"],
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
-        'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
     package_dir={"": "src"},
     entry_points={
-        'console_scripts':[
-            'catshand = catshand.main:main',
+        "console_scripts": [
+            "catshand = catshand.main:main",
         ]
     },
     packages=setuptools.find_packages(where="src"),
-    python_requires='>=3.9',
-    
+    python_requires=">=3.9",
 )
